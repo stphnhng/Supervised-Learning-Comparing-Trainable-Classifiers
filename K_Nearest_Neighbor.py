0e07aff115d479bcd734752950d24e1bad0f89ef
Christopher Sofian <cns24@uw.edu>
2018-03-04 11:00:05 -0800

    actually done LMAO

--- K_Nearest_Neighbor.py
+++ K_Nearest_Neighbor.py
@@ -18,7 +18,7 @@ def setTrainTestData(files, percentage, training = [], test = []):
 def euclideanDistance(data1, data2):
 	sum = 0
 	for i in range(4):
-		sum = sum + math.sqrt(math.pow(data1[i],2) + math.pow(data2[i],2))
+		sum = sum + math.sqrt(math.pow(data1[i] - data2[i],2))	
 	return sum
 
 def findNearestNeighbor(k, train, startingPoint):
@@ -27,15 +27,15 @@ def findNearestNeighbor(k, train, startingPoint):
 	for i in range(len(train)):
 		distance_cur = euclideanDistance(train[i], startingPoint)
 		distancesAll.append((distance_cur, train[i]))
-
 	distancesAll.sort(key = itemgetter(0))
 	for i in range(k):
 		nearestNodes.append(distancesAll[i][1])
 	return nearestNodes
 def classify(nearestNodes):
 	count = {}
-	for i in nearestNodes:
-		cur = i[-1]
+	print(nearestNodes)
+	for i in range(len(nearestNodes)):
+		cur = nearestNodes[i][-1]
 		if cur in count:
 			count[cur] = count[cur] + 1
 		else:
@@ -44,15 +44,19 @@ def classify(nearestNodes):
 	mostLikelyNum = -1
 	for i in count:
 		if(count[i] > mostLikelyNum):
+			print(i)
 			mostLikely = i
 			mostLikelyNum = count[i]
+
+	#print(mostLikely)
 	return mostLikely
 def check(test, answer):
 	count = 0
 	for i in range(len(test)):
 		if(answer[i] == test[i][-1]):
-			count = count + 1
-
+			print(answer[i])
+			print(test[i][-1])
+			count = count + 1	
 	return float(count)/float(len(test)) * 100
 
 
@@ -60,12 +64,14 @@ def main():
 	trainingSet = []
 	testSet = []
 	setTrainTestData('iris.data', 0.66, trainingSet, testSet)
-	k = 2
+	k = 3
 	predicted_value = []
-	for i in testSet:
-		nearestNeighbor = findNearestNeighbor(k, trainingSet, trainingSet[0])
+	for i in range(len(testSet)):
+		nearestNeighbor = findNearestNeighbor(k, trainingSet, testSet[i])
 		predicted_value.append(classify(nearestNeighbor))
+		print('> predicted=' + repr(classify(nearestNeighbor)) + ', actual=' + repr(testSet[i][-1]))
 	print(str(check(testSet, predicted_value)))
+
 	print("x")
 
 if __name__ == "__main__":
